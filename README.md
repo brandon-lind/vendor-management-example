e6b043e47d47f3deecbf541ffc8e4a55d05fd5b3
Brandon Lind <linddeveloper@gmail.com>
2020-08-22 11:56:17 -0500

    chore: updating packages

--- README.md
+++ README.md
@@ -1,20 +1,48 @@
 # Vendor Management Example
+
 A fictional project to practice creating a fully functional application use a
 variety of technologies such as Docker, Flyway, Postgesql, .NET Core, Java,
-Node, React, and Angular.
+Node, and React.
 
 ## Prerequisites
+
 The project assumes you have the following installed and configured locally:
 
 - Docker v18+
-- .NET Core v2.2
-- JDK v11 (v8 would probably work just fine too)
-- Maven v3.6
-- Node.js v10.15
+- .NET Core v3.1+
+- JDK v14 (v11 would probably work just fine too)
+- Maven v3.6+
+- Node.js v14+
 
-## Start up
+## Build It
+
+### UI (React)
+
+It's a standard Create React App.
+
+```
+(cd ./ui/vendormgmt-react && npm i)
+```
+
+### Java
+
+The Java API is a standard fair [Spring Boot](https://spring.io/projects/spring-boot) application using maven, so the normal process applies.
+
+From the command line:
+
+```
+(cd ./api/java && mvn clean install)
+```
+
+### .NET
+
+From the command line:
+(cd ./api/net/VendorManagement && dotnet build)
+
+## Start It
 
 ### Database
+
 To initialize and seed the database, execute the docker compose file in the infrastructure directory
 
 ```
@@ -22,6 +50,30 @@ docker-compose --f ./infrastructure/docker-compose.yml up
 ```
 
 ### Java
-The Java API is a standard fair [Spring Boot](https://spring.io/projects/spring-boot) application, so the normal process applies. It uses Lombok for some automatic getter/setter action, so be sure to install any IDE extensions to enable support.
+
+The Java API is a standard fair [Spring Boot](https://spring.io/projects/spring-boot) application using maven, so the normal process applies.
+From the command line:
+
+```
+(cd ./api/java && mvn spring-boot:run)
+```
+
+It uses Lombok for some automatic getter/setter action, so be sure to install any IDE extensions to enable support.
  - For VSCode: [Lombok Annotions Support for VS Code](https://marketplace.visualstudio.com/items?itemName=GabrielBB.vscode-lombok)
  - For Eclipse: Double click on the Lombok .jar in your .m2 directory after you have done an initial mvn clean install (~/.m2/repository/org/projectlombok/lombok/{version}/lombok-{version}.jar)
+
+### .NET
+
+The .NET API is also standard fair. Fire it up using the [dotnet CLI run command](https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-run)
+
+```
+(cd ./api/net/VendorManagement && dotnet run)
+```
+
+### UI (React)
+
+From the command line:
+
+```
+(cd ./ui/vendormgmt-react && npm start)
+```
--- api/java/src/test/java/com/brandonlind/vendormanagement/controller/VendorControllerTest.java
+++ api/java/src/test/java/com/brandonlind/vendormanagement/controller/VendorControllerTest.java
@@ -1,53 +1,43 @@
 package com.brandonlind.vendormanagement.controller;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
-import com.brandonlind.vendormanagement.model.Vendor;
-import com.brandonlind.vendormanagement.service.VendorRepo;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@RunWith(SpringRunner.class)
+import com.brandonlind.vendormanagement.model.Vendor;
+import com.brandonlind.vendormanagement.service.VendorRepo;
+
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(value = VendorController.class)
 @EnableAutoConfiguration()
-public class VendorControllerTest {
-
+class VendorControllerTest {
+	
 	@Autowired
 	private MockMvc mockMvc;
-
+	
 	@MockBean
 	private VendorRepo repo;
 
 	@Test
-	public void getAllVendors() throws Exception {
+	void getAllVendors() throws Exception {
 		// Setup the mock returned objects
 		List<Vendor> mockVendors = Arrays.asList(new Vendor(), new Vendor());
 
-		// Mock out the repo method response
+		// Mock out the repository method response
 		Mockito.when(repo.findAllByAndDeletedAtIsNull()).thenReturn(mockVendors);
 
 		// Stub out the request
@@ -62,29 +52,4 @@ public class VendorControllerTest {
 
 		JSONAssert.assertEquals(expected, actual, false);
 	}
-
-//	@Test
-//	public void addVendor() throws Exception {
-//		// Setup mock objects
-//		UUID vendorId = UUID.randomUUID();
-//		GeometryFactory geometryFactory = new GeometryFactory();
-//		Geometry vendorLocation = geometryFactory.createPoint(new Coordinate(30.466481, -97.844618));
-//		Vendor mockVendor = new Vendor(vendorId,"TEST01","Test Vendor",vendorLocation,null);
-//		Vendor requestVendor = new Vendor(null, "TEST01","Test Vendor", vendorLocation, null);
-//		String requestJson = new ObjectMapper().writeValueAsString(requestVendor);
-//		
-//		Mockito.when(repo.save(Mockito.any(Vendor.class))).thenReturn(mockVendor);
-//		
-//		RequestBuilder request = MockMvcRequestBuilders.post("/")
-//				.accept(MediaType.APPLICATION_JSON)
-//				.content(requestJson)
-//				.contentType(MediaType.APPLICATION_JSON);
-//		
-//		MvcResult result = mockMvc.perform(request).andReturn();
-//		
-//		MockHttpServletResponse response = result.getResponse();
-//		
-//		assertEquals(HttpStatus.CREATED.value(), response.getStatus());
-//		assertEquals("http://localhost/TEST01", response.getHeader(HttpHeaders.LOCATION));
-//	}
 }
